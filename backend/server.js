0e9a362218e965a37f91b9315980ba967d1005a9
vikram1155 <markiv1155@gmail.com>
2022-05-04 12:53:57 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -34,14 +34,14 @@ app.use("/api/notes", noteRoutes);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
 } else {
-  app.get("/", (req, res) => {
-    res.send("Api is running");
-  });
+    app.get("/", (req, res) => {
+        res.send("Api is running");
+    });
 }
 
 // deployment
